d507261424481dd48ae85e6b6992abaa82eeef53
MateusKoenig <mateuskoenig1@gmail.com>
2020-07-14 01:33:55 -0300

    Critado MCV do Departamento

--- HoraExtraWebApMVC/Models/ViewModels/ErrorViewModel.cs
+++ HoraExtraWebApMVC/Models/ViewModels/ErrorViewModel.cs
@@ -1,7 +1,7 @@
 using System;
 
-namespace HoraExtraWebApMVC.Models
-{
+namespace HoraExtraWebApMVC.Models.ViewModels
+{ 
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
