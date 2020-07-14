ba487a494ecec1ce2e3d1fcc867baf757d6294fd
MateusKoenig <mateuskoenig1@gmail.com>
2020-07-14 01:33:55 -0300

    Criado MCV do Departamento

---
+++ HoraExtraWebApMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HoraExtraWebApMVC.Models;
+using HoraExtraWebApMVC.Models.Departamento;
+
+namespace HoraExtraWebApMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            List<DepartamentoModel> list = new List<DepartamentoModel>();
+            list.Add(new DepartamentoModel { Id = 1, Nome = "TI", Gestor = "Dyu" });
+            return View(list);
+        }
+    }
+}
